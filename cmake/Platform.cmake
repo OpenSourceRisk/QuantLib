6e7c705689406507792353a6bbc76b1726bff06f
Matthias Groncki <matthias.groncki@acadia.inc>
2022-07-06 16:35:03 +0200

    QPR-9728 Allow static and dynamic linked runtimes with CMAKE using MSVC

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -1,17 +1,25 @@
 # Platform-specific flags and settings
-if (MSVC)
+option(MSVC_LINK_DYNAMIC_RUNTIME "Link against dynamic runtime" ON)
+option(MSVC_PARALLELBUILD "Use flag /MP" ON)
+
+if(MSVC)
     # See cmake policy CMP00091
     # One of "MultiThreaded", "MultiThreadedDebug", "MultiThreadedDLL", "MultiThreadedDebugDLL"
+    set(BUILD_SHARED_LIBS OFF)
+
+    # build static libs always
     set(CMAKE_MSVC_RUNTIME_LIBRARY
-        "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<BOOL:${BUILD_SHARED_LIBS}>:DLL>")
+        "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<BOOL:${MSVC_LINK_DYNAMIC_RUNTIME}>:DLL>")
 
     # Export all symbols so MSVC can populate the .lib and .dll
-    if (BUILD_SHARED_LIBS)
-        # Temp: disable DLL builds on MSVC
+    if(BUILD_SHARED_LIBS)
+        # # Temp: disable DLL builds on MSVC
         message(FATAL_ERROR
-            "Shared library (DLL) builds for QuantLib on MSVC are not supported")
+            <CONFIG>_OUTPUT_NAME "Shared library (DLL) builds for QuantLib on MSVC are not supported")
         set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
     endif()
+
+    add_definitions(/MP)
 
     add_compile_definitions(NOMINMAX)
 
@@ -20,6 +28,5 @@ if (MSVC)
 
     # /wd26812
     # Suppress warnings: "Prefer enum class over enum" (Enum.3)
-
     add_compile_options(/wd4267 /wd26812)
 endif()
